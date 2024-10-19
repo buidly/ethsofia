1342478e1e39f72864b2e512a1a3f20ba1b3976c
Gabriel Matei <gabrielvmatei@gmail.com>
2024-10-19 11:00:00 +0300

    homepage

--- frontend/src/pages/Explore/components/OraclesList.tsx
+++ frontend/src/pages/Explore/components/OraclesList.tsx
@@ -31,7 +31,7 @@ const OraclesListItem = ({ oracle }: { oracle: Oracle }) => {
   );
 }
 
-export const OraclesList = () => {
+export const OraclesList = ({ isHomePage }: { isHomePage?: boolean }) => {
   const [oracles, setOracles] = useState<Oracle[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -43,6 +43,10 @@ export const OraclesList = () => {
 
         const response = await axios.get(`${API_URL}/oracles`);
         setOracles(response.data);
+
+        if (isHomePage) {
+          setOracles(response.data.slice(0, 3))
+        }
       } catch (error) {
         console.error("Error fetching oracles", error);
         setError('Error fetching oracles');
@@ -85,15 +89,17 @@ export const OraclesList = () => {
       {oracles.map(oracle => (
         <OraclesListItem oracle={oracle} key={oracle._id} />
       ))}
-      <div className="p-10  rounded-3xl flex flex-row-reverse gap-6 justify-between bg-[#fdfdfc]">
-        <Link to={`/oracles/new`}>
-          <FontAwesomeIcon icon={faPlus} className="h-6 w-6" />
-        </Link>
-        <div className="flex flex-col gap-4">
-          <h2 className="text-2xl">Create new oracle</h2>
-          <p className="font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div >
+      {!isHomePage && (
+        <div className="p-10  rounded-3xl flex flex-row-reverse gap-6 justify-between bg-[#fdfdfc]">
+          <Link to={`/oracles/new`}>
+            <FontAwesomeIcon icon={faPlus} className="h-6 w-6" />
+          </Link>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl">Create new oracle</h2>
+            <p className="font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div >
+      )}
     </div >
   );
 };
--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,16 +1,23 @@
+import { Link } from "react-router-dom";
 import { NavBar } from "../../components";
+import { OraclesList } from "../Explore/components";
 
 export const HomePage = () => {
   return (
     <>
       <NavBar />
-      <div className="container m-auto p-6 flex flex-col gap-6">
-        <div className="flex items-center justify-center">
+      <div className="container m-auto p-6 flex flex-col gap-12 justify-center">
+        <div className="flex flex-col items-center text-center text-[#434343] gap-4">
           <img src="/snapdata-logo.png" alt="Logo" className="h-80 w-auto" />
+          <h1 className="text-3xl mt-[-5rem]">Create your first snap today</h1>
+          <p className="max-w-3xl font-normal">lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
-        <h1 className="text-3xl my-12">
-          data
-        </h1>
+        <OraclesList isHomePage />
+        <div>
+          <div className="flex items-center justify-center">
+            <Link to="/explore" className="p-4 px-6 border-2 border-[#212121] rounded-3xl">Explore more SNAPS</Link>
+          </div>
+        </div>
       </div>
     </>
   );
